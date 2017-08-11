4a3836e65c0dd767aebcd9106a4131d1b8733cfa
Simon Eddy <simon@simoneddy.com.au>
2017-08-11 20:57:02 +1000

    Create Destiny.php
    Beyonnnn

---
+++ Destiny.php
@@ -0,0 +1,29 @@
+<?php
+namespace Hax;
+class Destiny
+{
+    public $wonderfulMagic,
+           $powerfulWizard;
+    
+    public function __construct()
+    {
+        $this->powerfulWizard = 'Placeholder Wizard';
+        $this->_castCoolSpell('Get that Balrog');
+    }
+    
+    private function _castCoolSpell($coolSpell)
+    {
+        echo 'That '.$this->powerfulWizard.
+        ' begets the power to cast a '.$coolSpell.'.';
+        
+        if (!isset($this->wonderfulMagic)) {
+            $this->wonderfulMagic = array();
+        }
+        $this->wonderfulMagic[] = $coolSpell;
+    }
+    
+    public function findMeWiz()
+    {
+        var_dump($this->powerfulWizard);
+    }
+}
