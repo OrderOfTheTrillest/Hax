0f40f92c52835826cd93e4a6edf877151b3c183a
Simon Eddy <simon@simoneddy.com.au>
2017-10-07 01:40:01 +1100

    Create ripper.js

---
+++ learnyourself/ripper.js
@@ -0,0 +1,11 @@
+"use strict";
+
+class Ripper {
+    snorto(fint) {
+        console.log("Carn the " + fint + "s!");
+    }
+}
+
+var ripper = new Ripper();
+var fint = "docker";
+ripper.snorto(fint);
